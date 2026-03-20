f04c5b0a5b7321f6189ba4c2c409e3d4e2d5c82a
Jose Loaiza <jose.loaiza01@uceva.edu.co>
2026-03-20 00:48:56 -0500

    Ajustes para Railway

--- src/main/java/co/uceva/usuariosservice/infrastructure/config/SecurityConfig.java
+++ src/main/java/co/uceva/usuariosservice/infrastructure/config/SecurityConfig.java
@@ -13,6 +13,11 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
 
 @Configuration
 @EnableWebSecurity
@@ -42,7 +47,21 @@ public class SecurityConfig {
                 )
                 // AQUÍ ACTIVAMOS EL FILTRO: Revisa el token antes de dejar pasar la petición
                 .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class);
+        http.cors(cors -> cors.configurationSource(corsConfigurationSource()));
 
         return http.build();
+    }
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        // ⚠️ Permitir todo para que la App móvil no sea bloqueada
+        configuration.setAllowedOrigins(Arrays.asList("*"));
+        configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
+        configuration.setAllowedHeaders(Arrays.asList("Authorization", "Content-Type"));
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
     }
 }
