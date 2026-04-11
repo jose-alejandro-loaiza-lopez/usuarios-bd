a08e31d427f9ea1bc4858bc64a9c7576d069dfd9
Jose Loaiza <jose.loaiza01@uceva.edu.co>
2026-04-10 23:37:18 -0500

    fix: Solucionado un problema al iniciar sesion [EC-25]

--- src/main/java/co/uceva/usuariosservice/domain/model/EncryptedRequestDTO.java
+++ src/main/java/co/uceva/usuariosservice/domain/model/EncryptedRequestDTO.java
@@ -1,10 +1,11 @@
 package co.uceva.usuariosservice.domain.model;
 
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
 
 @Getter
 @Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class EncryptedRequestDTO {
     private String encryptedAesKey; // Cifrada con RSA (Base64)
     private String iv;              // Vector de inicialización (Base64)
